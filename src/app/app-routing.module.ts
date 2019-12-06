07f176e53af3e7011488db75d5e513e9661b5da5
aurso74 <46983022+aurso74@users.noreply.github.com>
2019-12-06 12:35:02 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,6 @@ import { AppComponent } from './app.component';
 import { BookmarkFrmComponent } from './bookmark-frm/bookmark-frm.component';
 import { BookmarkmainComponent } from './bookmarkmain/bookmarkmain.component';
 
-/*const routes: Routes = [
-  { path: 'bookmarks', component: AppComponent },
-  { path: 'bookmark-frm', component: BookmarkFrmComponent },
-  { path: '', redirectTo: '/bookmarks', pathMatch: 'full' }
-];*/
-
 const routes: Routes = [
   { path: 'bookmark-frm', component: BookmarkFrmComponent },
   { path: 'bookmarkmain', component: BookmarkmainComponent},
